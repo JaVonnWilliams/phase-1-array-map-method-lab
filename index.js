f481dd46b238bd33b627b2ef5c3a1604c10879c3
JaVonn Williams <javonn_w@yahoo.com>
2022-12-19 16:46:24 -0800

    commit tree e3590eb44a84f37d7a19ab1d950d2340a759e914

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials.map(text) => {
+//     const sentances = text.split(' ');
+//     const capitalizedSentences = sentances.map(
+//       (sentances) => sentances.charAt(0).toUpperCase() + sentances.slice(1)
+//       );
+    
+//   };
+//   const  response = capitalizedSentences.join(" ");
+//   return response
+// };
+
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
+    return response;
+  });
+};
